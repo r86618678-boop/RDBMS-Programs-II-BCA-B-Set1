ca0ae0863c5cde23ee35abfb2cea14592be0d130
ramasamy-1U25CA072 <r86618678@gmail.com>
2026-08-27 11:30:36 +0530

    Update solution.sql

--- Assignment02_Create_Student/solution.sql
+++ Assignment02_Create_Student/solution.sql
@@ -1,6 +1,8 @@
-DROP DATABASE IF EXISTS CollegeDB;
-CREATE DATABASE CollegeDB;
-USE CollegeDB;
+USE collegeDBBCA;
+CREATE TABLE Course (courseID varchar(20),COURSENAME varchar(20),credits int(20) PRIMARY KEY,DepartmentID VARCHAR(20));
+insert into Course(courseID,courseNAME,Credits,departmentID) values (10,"BCA", 12,1);
+DESC Course;
+
 
 -- Create Student table
 
